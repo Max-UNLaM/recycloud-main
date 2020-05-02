30011b3b2d8fefb2c53d7594d3f4171fcd56feb3
Maximiliano De Pietro <maximiliano.depietro@gmail.com>
2020-05-01 21:42:02 -0300

    Application

---
+++ src/test/java/ar/edu/unlam/recycloud/RecycloudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unlam.recycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecycloudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
