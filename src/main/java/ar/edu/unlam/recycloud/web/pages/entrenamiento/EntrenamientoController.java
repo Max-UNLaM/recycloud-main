2db54c44c3ed6e8510391807123d2f662c8ba2c4
NicolasMorales <nicolasemanuelmroales@hotmail.com>
2020-05-03 15:48:45 -0300

    david 2

--- src/main/java/ar/edu/unlam/recycloud/web/pages/entrenamiento/EntrenamientoController.java
+++ src/main/java/ar/edu/unlam/recycloud/web/pages/entrenamiento/EntrenamientoController.java
@@ -1,13 +1,22 @@
 package ar.edu.unlam.recycloud.web.pages.entrenamiento;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EntrenamientoController {
 
+    public String a;
+
     @GetMapping("/entrenamiento")
     public String scanner() {
+        return "/entrenamiento/entrenamiento";
+    }
+
+    @RequestMapping(path = "/entrenamiento/guardarEntrenamiento", method = RequestMethod.POST)
+    public String guardar (@ModelAttribute String clasificador) {
+        this.a=clasificador;
         return "/entrenamiento/entrenamiento";
     }
 }
