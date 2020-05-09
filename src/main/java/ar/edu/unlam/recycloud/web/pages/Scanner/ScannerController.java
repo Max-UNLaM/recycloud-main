6fce8be520d7025bbb32e31d5a360512a52af9b6
NicolasMorales <nicolasemanuelmroales@hotmail.com>
2020-05-09 03:05:28 -0300

    version piola

--- src/main/java/ar/edu/unlam/recycloud/web/pages/Scanner/ScannerController.java
+++ src/main/java/ar/edu/unlam/recycloud/web/pages/Scanner/ScannerController.java
@@ -1,6 +1,7 @@
 package ar.edu.unlam.recycloud.web.pages.Scanner;
 
-import ar.edu.unlam.recycloud.web.pages.entrenamiento.EntrenamientoService;
+import ar.edu.unlam.recycloud.web.pages.categorias.categoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,14 +10,17 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class ScannerController {
 
-    private final EntrenamientoService entrenamientoService;
+    @Autowired
+    private final categoriaService categoriaService;
 
-    ScannerController(EntrenamientoService entrenamientoService) {
-        this.entrenamientoService = entrenamientoService;
+    public ScannerController( categoriaService categoriaService) {
+        this.categoriaService = categoriaService;
     }
 
     @RequestMapping(path = "/scanner/scanner")
     public ModelAndView read() {
-        return new ModelAndView("scanner/scanner");
+        ModelMap mod = new ModelMap();
+        mod.put("categorias",this.categoriaService.getListaDeCategoria());
+        return new ModelAndView("scanner/scanner", mod);
     }
 }
