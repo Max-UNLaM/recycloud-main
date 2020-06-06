d3071c67a4a88200da4da607ca77d813a329067f
Davidb <davidb@epidataconsulting.com>
2020-06-06 16:43:33 -0300

    Crear Evento y Listar

--- src/main/java/ar/edu/unlam/recycloud/web/pages/eventos/EventoController.java
+++ src/main/java/ar/edu/unlam/recycloud/web/pages/eventos/EventoController.java
@@ -2,6 +2,7 @@ package ar.edu.unlam.recycloud.web.pages.eventos;
 
 import ar.edu.unlam.recycloud.app.evento.Evento;
 import ar.edu.unlam.recycloud.app.evento.EventoService;
+import ar.edu.unlam.recycloud.web.pages.login.RegisterModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -25,8 +26,10 @@ public class EventoController {
         return new ModelAndView("/eventos/eventos", model);
     }
     @GetMapping(path = "/eventos/crear")
-    public String crear(Evento evento) {
-        return "/eventos/crear";
+    public ModelAndView crear() {
+        ModelMap modelo = new ModelMap();
+        modelo.addAttribute("evento", new Evento());
+        return new ModelAndView("/eventos/crear", modelo);
     }
 
     @PostMapping(path = "/eventos/crear")
